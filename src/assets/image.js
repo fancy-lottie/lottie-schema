172044f6eacc923d917be96c8603b60706928c30
chenpx976 <chenpx976@gmail.com>
2019-03-06 18:04:30 +0800

    feat: 添加修改背景功能

---
+++ src/assets/image.js
@@ -0,0 +1,11 @@
+import {
+  fromJS
+} from 'immutable';
+export default new fromJS({
+  "id": "bgImage",
+  "w": 1024,
+  "h": 350,
+  "u": "",
+  "p": "",
+  "e": 1
+})
---
+++ src/layers/image.js
@@ -0,0 +1,45 @@
+import {
+  fromJS
+} from 'immutable';
+export default new fromJS({
+  "ddd": 0,
+  "ind": 1,
+  "ty": 2,
+  "nm": "背景图片",
+  "refId": "bgImage",
+  "cl": "handlehook",
+  "ln": "bgImage",
+  "sr": 1,
+  "ks": {
+    "o": {
+      "a": 0,
+      "k": 100,
+      "ix": 11
+    },
+    "r": {
+      "a": 0,
+      "k": 0,
+      "ix": 10
+    },
+    "p": {
+      "a": 0,
+      "k": [512, 175, 0],
+      "ix": 2
+    },
+    "a": {
+      "a": 0,
+      "k": [512, 175, 0],
+      "ix": 1
+    },
+    "s": {
+      "a": 0,
+      "k": [100, 100, 100],
+      "ix": 6
+    }
+  },
+  "ao": 0,
+  "ip": 0,
+  "op": 750,
+  "st": 0,
+  "bm": 0
+})
